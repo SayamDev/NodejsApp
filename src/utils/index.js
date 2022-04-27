89441608e2d4cdc6679f78bd5dedf45f18b81a59
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-27 12:28:18 +0100

    CRUD - Update working

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,38 @@
-exports.addMovie = async ( collection, movieObj ) => {
+exports.addMovie = async (collection, movieObj) => {
     try {
-        const addEntry = await collection.insertOne(movieObj)
-        console.log(addEntry)
+        const addEntry = await collection.insertOne(movieObj);
+        console.log(addEntry);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
+
+exports.deleteMovie = async (collection, movieObj) => {
+    try {
+        const deleteEntry = await collection.deleteOne(movieObj);
+        console.log(deleteEntry);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.updateMovie = async (collection, movieObj) => {
+    try {
+        const updateEntry = await collection.updateOne(movieObj);
+        console.log(updateEntry);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 exports.listMovies = async (collection) => {
     try {
-        const movieList = await collection.find().toArray()
-        console.log(movieList)
+        const movieList = await collection.find().toArray();
+        console.log(movieList);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
+
 
 
