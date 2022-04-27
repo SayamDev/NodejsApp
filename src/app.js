03c7ef8e37135631ba0725f382f55abd8dedaf57
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-27 12:28:30 +0100

    update working

--- src/app.js
+++ src/app.js
@@ -1,24 +1,43 @@
-const yargs = require("yargs")
+const yargs = require("yargs");
 
-const { connection, client } = require("./db/connection")
-const { addMovie, listMovie, listMovies } = require("./utils")
+const { connection, client } = require("./db/connection");
+const { addMovie, listMovies, deleteMovie, updateMovie } = require("./utils");
 
 const app = async (yargsObj) => {
-    const collection = await connection()
+    const collection = await connection();
     if (yargsObj.add) {
-        await addMovie(collection, { title: yargsObj.title, actors: yargsObj.actors , year: yargsObj.year })
-        console.log("sucessfully added new document to db")
+        await addMovie(collection, { title: yargsObj.title, actor: yargsObj.actor});
+        console.log("Successfully added new document to db")
     } else if (yargsObj.list) {
         await listMovies(collection)
+    } else if (yargsObj.delete) {
+        await deleteMovie(collection , { title: yargsObj.title, actor: yargsObj.actor})
+        console.log("Successfully deleted from db")
+    } else if (yargsObj.update) {
+        await updateMovie(collection , { title: yargsObj.title, actor: yargsObj.actor})
+        console.log("Successfully updated an entry from db")
     } else {
         console.log("incorrect Command")
     }
-    await client.close()
-}
+    await client.close;
+};
 
-app(yargs.argv)
-
+app(yargs.argv);
 
 //LO : do the full complete CRUD operation
 //so far done CR
 //need to do UD (update and Delete - use mongo db docs)
+
+
+
+// else if (yargsObj.delete) {
+//     await deleteMovie(collection, { title: yargsObj.title, actors: yargsObj.actors, year: yargsObj.year });
+//      console.log("sucessfully added new document to db")
+
+
+
+
+
+
+
+
