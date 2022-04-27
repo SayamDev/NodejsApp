2c5e4585129f2f6b06c937347e559be03a224f1c
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-27 13:50:55 +0100

    added year

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ const { addMovie, listMovies, deleteMovie, updateMovie } = require("./utils");
 const app = async (yargsObj) => {
     const collection = await connection();
     if (yargsObj.add) {
-        await addMovie(collection, { title: yargsObj.title, actor: yargsObj.actor});
+        await addMovie(collection, { title: yargsObj.title, actor: yargsObj.actor, year:yargsObj.year});
         console.log("Successfully added new document to db")
     } else if (yargsObj.list) {
         await listMovies(collection)
     } else if (yargsObj.delete) {
-        await deleteMovie(collection , { title: yargsObj.title, actor: yargsObj.actor})
+        await deleteMovie(collection , { title: yargsObj.title, actor: yargsObj.actor,  year:yargsObj.year})
         console.log("Successfully deleted from db")
     } else if (yargsObj.update) {
-        await updateMovie(collection , { title: yargsObj.title, actor: yargsObj.actor})
+        await updateMovie(collection , { title: yargsObj.title, actor: yargsObj.actor, year:yargsObj.year})
         console.log("Successfully updated an entry from db")
     } else {
         console.log("incorrect Command")
